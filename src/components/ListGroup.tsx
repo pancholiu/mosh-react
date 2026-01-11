e3c92d46b41cf82d12b07390fa59c4859eb7157d
Francisco Liu <pancholiu@hotmail.com>
2026-01-10 23:07:15 -0400

    Building Components: 6 - Handling Events

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["NY", "SF", "Tokyo", "London", "Paris"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
