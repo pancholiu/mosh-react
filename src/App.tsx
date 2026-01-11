c7f222600819617faf7ded279e0631945bff0bf6
Francisco Liu <pancholiu@hotmail.com>
2026-01-11 18:39:40 -0400

    Building components: 14 - Excercise - Showing an Aler

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
-    <div className="alert alert-primary">
-      <Alert>
-        Hello <span>world!</span>
-      </Alert>
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
+        My button
+      </Button>
     </div>
   );
 }
