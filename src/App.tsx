5dcdf352b0f056a6511b80cec5aca7dcfcdc3d1c
Francisco Liu <pancholiu@hotmail.com>
2026-01-31 23:11:47 -0400

    Building expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "d1", amount: 10, category: "Utilities" },
+    { id: 2, description: "d2", amount: 20, category: "Utilities" },
+    { id: 3, description: "d3", amount: 30, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
