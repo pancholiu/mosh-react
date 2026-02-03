972214f64ebc0930c80e9c8f3d6af15791030626
Francisco Liu <pancholiu@hotmail.com>
2026-02-03 17:37:33 -0400

    Building Forms: 14 - Integrating with React Hook Forms and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
