6ea3a32a1f2cb7854083425157a3dbf75ef29420
Francisco Liu <pancholiu@hotmail.com>
2026-02-02 22:09:13 -0400

    Building Forms: 13 - Building the Expense Form

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { categories } from "../../App";
 
 interface Props {
   onSelectCategory: (category: string) => void;
@@ -10,9 +10,12 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
       className="form-select"
       onChange={(event) => onSelectCategory(event.target.value)}
     >
-      <option value="Groceries">Groceries</option>
-      <option value="Utilities">Utilities</option>
-      <option value="Entertainment">Entertainment</option>
+      <option value="">All categories</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
     </select>
   );
 };
