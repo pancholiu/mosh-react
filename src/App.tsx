e2a6e18dc72daf18b29b3e77a5a59d2d44c1fc4f
Francisco Liu <pancholiu@hotmail.com>
2026-02-09 16:48:41 -0400

    Connecting to the Backend: 4 - Effect clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [category, setCategory] = useState("");
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "d1", amount: 10, category: "Utilities" },
-    { id: 2, description: "d2", amount: 20, category: "Utilities" },
-    { id: 3, description: "d3", amount: 30, category: "Utilities" },
-  ]);
+  useEffect(() => {
+    connect();
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+    return () => disconnect();
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
